cdf33874f2d1521ab19c943d4d5cb1a23f8bd37d
SoniKurian <129376373+SoniaKurian@users.noreply.github.com>
2023-04-27 20:26:17 +0100

    Ticket service and exception implementation

--- cinema-tickets-java/src/main/java/uk/gov/dwp/uc/pairtest/TicketServiceImpl.java
+++ cinema-tickets-java/src/main/java/uk/gov/dwp/uc/pairtest/TicketServiceImpl.java
@@ -13,49 +13,55 @@ public class TicketServiceImpl implements TicketService {
     {
         if(accountId < = 0 ){
             //Throw exception - not a avalid account
+            throw new InvalidPurchaseException("Invalid accountId");
         }
 
         boolean hasAdult = false;
         int totalPrice = 0;
         int totalTickets = 0; 
+        int maximumTickets = 0;
         // Loop through each ticket types
         for (TicketTypeRequest tickets : ticketTypeRequests)
         {
             var type =  tickets.getTicketType();
             var numberOfTickets = tickets.getNoOfTickets();
-
-            //check an adult exists in the request
-            if(type == Type.ADULT)
-            {
-                hasAdult = true; 
-            }
+            //
+            maximumTickets = maximumTickets + numberOfTickets;
             
-            //Get the total number of tickets to check if it has exceeded the max allowed
-            totalTickets = totalTickets + numberOfTickets
             if(numberOfTickets > 0)
             {
+                //Only Adult and Child need to pay for a ticket and seat allocated
                 if (type == Type.ADULT)
                 {
+                    hasAdult = true; //An adult exists in the request
+                    totalTickets = totalTickets + numberOfTickets
                     totalPrice = totalPrice +  (numberOfTickets * 20)
                 }
                 else if (type == Type.CHILD)
                 {
+                    totalTickets = totalTickets + numberOfTickets
                     totalPrice = totalPrice +  (numberOfTickets * 10)
                 }
             }
         }
 
-        if(numberOfTickets > 20 ){
+        if(maximumTickets > 20 ){
             //throw exception 
+            throw new InvalidPurchaseException("Only a maximum of 20 tickets that can be purchased at a time");
         }
 
         if(hasAdult == false){
             //throw exception 
+            throw new InvalidPurchaseException("Child and Infant tickets cannot be purchased without purchasing an Adult ticket");
         }
 
         if(numberOfTickets <= 20 && hasAdult){
             //reserve seat 
+            SeatReservationService reservationService = new SeatReservationServiceImpl();
+            reservationService.reserveSeat(accountId,numberOfTickets )
             //Make payment 
+            TicketPaymentService paymentService = new TicketPaymentServiceImpl();
+            paymentService.makePayment(accountId, totalPrice);
         }
         
     }
--- cinema-tickets-java/src/main/java/uk/gov/dwp/uc/pairtest/exception/InvalidPurchaseException.java
+++ cinema-tickets-java/src/main/java/uk/gov/dwp/uc/pairtest/exception/InvalidPurchaseException.java
@@ -2,4 +2,9 @@ package uk.gov.dwp.uc.pairtest.exception;
 
 public class InvalidPurchaseException extends RuntimeException {
 
+    //Constructor that accepts a message
+    public InvalidPurchaseException(String message){
+        super(message);
+    }
+
 }
